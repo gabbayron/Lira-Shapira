ce4c029f1c41c12cd84e3aefdd7302684c3c1141
gabbayron <gabbayron2307@gmail.com>
2020-07-23 17:26:34 +0300

    init

---
+++ src/pages/MakeTransaction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const MakeTransaction = () => {
+    return (
+        <>
+        <h1>User Page</h1>
+        <Link to="/">User Page</Link>
+        </>
+    );
+}
+
+export default MakeTransaction;
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+const totalAmount = 30;
+const transactions = [
+    {    }
+]
+
+const UserPage = () => {
+
+
+    return ( 
+        <>
+        <h1>Hello</h1>
+        <Link to="/MakeTransaction">Make Transaction</Link>
+        </>
+     );
+}
+ 
+export default UserPage;
