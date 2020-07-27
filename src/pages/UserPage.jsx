b60e6013a0d8e91b400ba4d07cde0f2233bf5764
gabbayron <gabbayron2307@gmail.com>
2020-07-27 14:48:18 +0300

    remove unused

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Tables } from '../components/Tables.jsx'
-import { transactions } from '../components/transactions.jsx'
 
 
 const totalAmount = 30;
 
 const UserPage = () => {
-
-
     return (
         <>
             <div className="jumbotron jumbotron-fluid text-center">
